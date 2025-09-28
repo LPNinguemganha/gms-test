8eb29ffc5327e19ad5ff3b37a4d00a6a1ecd8a73
Joao Paulo <autoflux8@gmail.com>
2025-09-28 01:32:18 -0300

    refatoração e teste de busca

---
+++ cypress/e2e/cadastro2.cy.js
@@ -0,0 +1,19 @@
+///<reference types="cypress"/>
+
+
+describe('US-012-Funcionalidade: Cadastro de membros', () => {
+beforeEach(() => {
+  cy.visit('http://localhost:3000')
+});
+
+  it('Deve fazer o cadastro de campos obrigatorios', () => {
+    var email = `fabio${Date.now()}@teste.com`
+    cy.preencherCadastro('Fabio' , 'Araujo' , email, '11262626262' , 'Teste@122335' )
+    cy.get('#signup-response').should("contain" , "Cadastro realizado com sucesso!")
+  })
+
+  it('Deve validar mensagem de erro com o campo nome invalido', () => {
+    cy.preencherCadastro('Fabio20' , 'Araujo' , 'fabio@teste.com' , '1126262626' , 'Teste@122335')
+    cy.get('#signup-response').should('contain', 'Nome deve conter apenas caracteres alfabéticos')
+  })
+})
